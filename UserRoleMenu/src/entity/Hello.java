88a0133c176ecaeab2df082fd474dd6379ba9454
chanming2015 <476076812@qq.com>
2015-03-10 16:31:24 +0800

    Second Commit

---
+++ UserRoleMenu/src/entity/Hello.java
@@ -0,0 +1,26 @@
+/*
+ *www.dyr.com
+ *Copyright (c) 2014 All Rights Reserved
+ */
+/**
+ * Author XuMaoSen
+ */
+package entity;
+
+/**
+ * Project:UserRoleMenu
+ * Package:entity
+ * FileName:Hello.java
+ * Comments:
+ * JDK Version:
+ * Author XuMaoSen
+ * Create Date:2015-3-10 下午4:31:08
+ * Modified By:XuMaoSen
+ * Modified Time:
+ * What is Modified:
+ * Description:
+ * Version:
+ */
+public class Hello {
+
+}
